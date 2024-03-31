8d2fb51e4862a1d89d0dfeae40ac7292265e95b4
roychshao <roych.shao@gmail.com>
2024-04-01 01:20:26 +0800

    [feat, bug] timestamp connection done, but file pasing wrong.

--- custom-otelcol/socketSidecar/index.js
+++ custom-otelcol/socketSidecar/index.js
@@ -11,43 +11,127 @@ const io = socketIO(server);
 io.on('connection', (socket) => {
     console.log("connection build");
 
-    socket.on('requestTraces', () => {
-        const tracesFilePath = path.join('/tmp', 'otelcol', 'file_exporter', 'traces.json');
-        fs.readFile(tracesFilePath, (err, data) => {
-            console.log('trace file path', tracesFilePath);
+    socket.on('requestTraces', (timestamp) => {
+
+        const directoryPath = path.join('/tmp', 'otelcol/file_exporter');
+        fs.readdir(directoryPath, (err, files) => {
             if (err) {
-                console.error('Error reading log file:', err);
+                console.error("Could not list the directory.", err);
                 return;
             }
-            console.log('Received request for traces');
-            socket.emit('sendTraces', data);
+            // filter files
+            const filteredFiles = files.filter(file => {
+
+                const timestampRegex = /traces-(\d{4}-\d{2}-\d{2}T\d{2}-\d{2}-\d{2})\.\d{3}\.json/;
+                const match = file.match(timestampRegex);
+
+                if (!match) {
+                    return false;
+                }
+
+                const fileTimestamp = match[1];
+                return fileTimestamp > timestamp;
+            });
+            // emit with timestamp
+            socket.emit('sendTraces', {
+                files: filteredFiles,
+                timestamp: new Date().toISOString()
+            });
         })
+
+        // const tracesFilePath = path.join('/tmp', 'otelcol', 'file_exporter', 'traces.json');
+        // fs.readFile(tracesFilePath, (err, data) => {
+        //     console.log('trace file path', tracesFilePath);
+        //     if (err) {
+        //         console.error('Error reading log file:', err);
+        //         return;
+        //     }
+        //     console.log('Received request for traces');
+        //     socket.emit('sendTraces', data);
+        // })
     })
 
-    socket.on('requestMetrics', () => {
-        const metricsFilePath = path.join('/tmp', 'otelcol', 'file_exporter', 'metrics.json');
-        fs.readFile(metricsFilePath, (err, data) => {
+    socket.on('requestMetrics', (timestamp) => {
+
+        console.log("receive requestMetrics request");
+        console.log("timestamp: " + timestamp);
+        const directoryPath = path.join('/tmp', 'otelcol/file_exporter');
+        fs.readdir(directoryPath, (err, files) => {
             if (err) {
-                console.error('Error reading log file:', err);
+                console.error("Could not list the directory.", err);
                 return;
             }
+            // filter files 
+            const filteredFiles = files.filter(file => {
 
-            console.log('Received request for metrics');
-            socket.emit('sendMetrics', data);
+                const timestampRegex = /metrics-(\d{4}-\d{2}-\d{2}T\d{2}-\d{2}-\d{2})\.\d{3}\.json/;
+                const match = file.match(timestampRegex);
+
+                if (!match) {
+                    return false;
+                }
+
+                const fileTimestamp = match[1];
+                return fileTimestamp > timestamp;
+            });
+            // emit with timestamp
+            console.log("filter process finished, start emitting...");
+            console.log(filteredFiles);
+            socket.emit('sendTraces', {
+                files: filteredFiles,
+                timestamp: new Date().toISOString()
+            });
         })
+        // const metricsFilePath = path.join('/tmp', 'otelcol', 'file_exporter', 'metrics.json');
+        // fs.readFile(metricsFilePath, (err, data) => {
+        //     if (err) {
+        //         console.error('Error reading log file:', err);
+        //         return;
+        //     }
+        //
+        //     console.log('Received request for metrics');
+        //     socket.emit('sendMetrics', data);
+        // })
     })
 
-    socket.on('requestLogs', () => {
-        const logsFilePath = path.join('/tmp', 'otelcol', 'file_exporter', 'logs.json');
-        fs.readFile(logsFilePath, (err, data) => {
+    socket.on('requestLogs', (timestamp) => {
+
+        const directoryPath = path.join('/tmp', 'otelcol/file_exporter');
+        fs.readdir(directoryPath, (err, files) => {
             if (err) {
-                console.error('Error reading log file:', err);
+                console.error("Could not list the directory.", err);
                 return;
             }
+            // filter files
+            const filteredFiles = files.filter(file => {
 
-            console.log('Received request for logs');
-            socket.emit('sendLogs', data);
+                const timestampRegex = /logs-(\d{4}-\d{2}-\d{2}T\d{2}-\d{2}-\d{2})\.\d{3}\.json/;
+                const match = file.match(timestampRegex);
+
+                if (!match) {
+                    return false;
+                }
+
+                const fileTimestamp = match[1];
+                return fileTimestamp > timestamp;
+            });
+            // emit with timestamp
+            socket.emit('sendTraces', {
+                files: filteredFiles,
+                timestamp: new Date().toISOString()
+            });
         })
+
+        // const logsFilePath = path.join('/tmp', 'otelcol', 'file_exporter', 'logs.json');
+        // fs.readFile(logsFilePath, (err, data) => {
+        //     if (err) {
+        //         console.error('Error reading log file:', err);
+        //         return;
+        //     }
+        //
+        //     console.log('Received request for logs');
+        //     socket.emit('sendLogs', data);
+        // })
     })
 });
 
--- pulling-server/index.js
+++ pulling-server/index.js
@@ -1,58 +1,116 @@
-const socketIOClient = require('socket.io-client');
-const socket = socketIOClient('http://10.96.20.33:3000');
-const fs = require('fs');
-const readline = require('readline');
+import socketIOClient from 'socket.io-client';
+const socket = socketIOClient('http://10.103.7.125:3000');
+import fs from 'fs';
+import readline from 'readline';
+import path from 'path';
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
+// TODO: file format wrong, file.content and file.name undefined.
+// TODO: make the timestamp transmitted be little later
+
+var tracesTimestamp = new Date().getTime();
+var metricsTimestamp = new Date().toISOString();
+var logsTimestamp = new Date().toISOString();
+
 socket.on('connect', () => {
   console.log('Connected to telemetry server');
   rl.on('line', (input) => {
     if (input === 'requestTraces') {
-      socket.emit('requestTraces');
+      socket.emit('requestTraces', new Date().toISOString());
     }
     if (input === 'requestMetrics') {
-      socket.emit('requestMetrics');
+      socket.emit('requestMetrics', new Date().toISOString());
     }
     if (input === 'requestLogs') {
-      socket.emit('requestLogs');
+      socket.emit('requestLogs', new Date().toISOString());
     }
   });
 });
 
-socket.on('sendTraces', (traces) => {
-    fs.writeFileSync('./receivedData/traces.json', traces, (err) => {
-        if (err) {
-            console.log(`Error writing traces to file: ${err}`);
-        } else {
-            console.log('Traces written to file successfully.');
-        }
+socket.on('sendTraces', (data) => {
+    
+    tracesTimestamp = data.timestamp;
+
+    data.files.forEach(file => {
+        const fileContent = Buffer.from(file.content, 'base64');
+        const filePath = path.join('./receivedData/', file.name);
+
+        fs.writeFileSync(filePath, fileContent, (err) => {
+            if (err) {
+                console.error(`Failed to save file ${file.name}:`, err);
+            } else {
+                console.log(`File ${file.name} saved successfully.`);
+            }
+        })
     });
-    console.log('Traces received.')
+    // fs.writeFileSync('./receivedData/traces.json', traces, (err) => {
+    //     if (err) {
+    //         console.log(`Error writing traces to file: ${err}`);
+    //     } else {
+    //         console.log('Traces written to file successfully.');
+    //     }
+    // });
+    // console.log('Traces received.')
 });
 
-socket.on('sendMetrics', (metrics) => {
-    fs.writeFileSync('./receivedData/metrics.json', metrics, (err) => {
-        if (err) {
-            console.log(`Error writing metrics to file: ${err}`);
-        } else {
-            console.log('Metrics written to file successfully.');
-        }
-    })
-    console.log('Metrics received.');
+socket.on('sendMetrics', (data) => {
+  
+    console.log("receive sendMetrics response");
+    console.log(data);
+    metricsTimestamp = data.timestamp;
+
+    data.files.forEach(file => {
+        console.log(file);
+        const fileContent = Buffer.from(file.content, 'base64');
+        const filePath = path.join('./receivedData/', file.name);
+
+        fs.writeFileSync(filePath, fileContent, (err) => {
+            if (err) {
+                console.error(`Failed to save file ${file.name}:`, err);
+            } else {
+                console.log(`File ${file.name} saved successfully.`);
+            }
+        })
+    });
+
+    // fs.writeFileSync('./receivedData/metrics.json', metrics, (err) => {
+    //     if (err) {
+    //         console.log(`Error writing metrics to file: ${err}`);
+    //     } else {
+    //         console.log('Metrics written to file successfully.');
+    //     }
+    // })
+    // console.log('Metrics received.');
 });
 
-socket.on('sendLogs', (logs) => {
-    fs.writeFileSync('./receivedData/logs.json', logs, (err) => {
-        if (err) {
-            console.log(`Error writing logs to file: ${err}`);
-        } else {
-            console.log('Logs written to file successfully.');
-        }
-    })
-    console.log('Logs received.')
+socket.on('sendLogs', (data) => {
+
+    logsTimestamp = data.timestamp;
+
+    data.files.forEach(file => {
+        const fileContent = Buffer.from(file.content, 'base64');
+        const filePath = path.join('./receivedData/', file.name);
+
+        fs.writeFileSync(filePath, fileContent, (err) => {
+            if (err) {
+                console.error(`Failed to save file ${file.name}:`, err);
+            } else {
+                console.log(`File ${file.name} saved successfully.`);
+            }
+        })
+    });
+
+    // fs.writeFileSync('./receivedData/logs.json', logs, (err) => {
+    //     if (err) {
+    //         console.log(`Error writing logs to file: ${err}`);
+    //     } else {
+    //         console.log('Logs written to file successfully.');
+    //     }
+    // })
+    // console.log('Logs received.')
 });
 
