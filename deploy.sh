67568bd1bba73d4e7b8db651c2487848b4719c4a
roychshao <roych.shao@gmail.com>
2024-06-11 20:42:39 +0800

    [fix] cancel file storage seperation from deployment and daemonset since diffrent pod.

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ export DOCKERHUB_USERNAME=roychshao
 export IMAGE_NAME=sunbird-ap-amd64
 # docker inspect -f '{{.NetworkSettings.IPAddress}}' mysql_node
 # export MYSQL_ADDR=$(docker inspect -f '{{.NetworkSettings.IPAddress}}' mysql_node)
-export MYSQL_ADDR=192.168.43.182
+export MYSQL_ADDR=192.168.101.128
 
 envsubst < ./cluster-configs/mysqlagent.yaml | kubectl apply -f -
 envsubst < ./cluster-configs/tomcat.yaml | kubectl apply -f -
